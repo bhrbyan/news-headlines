6477f5484f15a02a8d1082b538781186c20dad42
bhrbyan <by.devlpr@gmail.com>
2023-07-14 18:06:20 +0700

    Add image article

--- features/feature-article/src/main/java/com/news/feature/article/ScreenArticle.kt
+++ features/feature-article/src/main/java/com/news/feature/article/ScreenArticle.kt
@@ -3,21 +3,17 @@ package com.news.feature.article
 import android.text.TextUtils
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -27,6 +23,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.google.android.material.textview.MaterialTextView
 import com.news.data.article.model.Article
 import com.news.ui.R
@@ -94,8 +91,7 @@ fun ArticleItem(
     modifier: Modifier = Modifier,
     onClickArticle: (url: String) -> Unit,
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Column(
         modifier = modifier
             .padding(20.dp)
             .fillMaxWidth()
@@ -103,47 +99,44 @@ fun ArticleItem(
                 onClickArticle(article.url)
             }
     ) {
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            Text(
-                text = article.title,
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            if (article.description != null) {
-                val spannedText =
-                    HtmlCompat.fromHtml(article.description ?: "", HtmlCompat.FROM_HTML_MODE_LEGACY)
-
-                AndroidView(
-                    factory = { MaterialTextView(it) },
-                    update = {
-                        it.text = spannedText
-                        it.maxLines = 2
-                        it.ellipsize = TextUtils.TruncateAt.END
-                    },
-                )
-            }
-
-            if (article.author.isNullOrEmpty().not()) {
-                Text(
-                    text = article.author ?: "",
-                    modifier = Modifier.padding(end = 8.dp, top = 8.dp)
-                )
-            }
-            Text(
-                text = article.publishedAt,
-                fontWeight = FontWeight.Light,
-                fontSize = 12.sp
-            )
-
-        }
-        Icon(
-            imageVector = Icons.Rounded.KeyboardArrowRight,
-            contentDescription = null
+        AsyncImage(
+            model = article.urlToImage,
+            contentDescription = null,
+            contentScale = ContentScale.Fit
         )
+        Text(
+            text = article.title,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        if (article.description != null) {
+            val spannedText =
+                HtmlCompat.fromHtml(article.description ?: "", HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+            AndroidView(
+                factory = { MaterialTextView(it) },
+                update = {
+                    it.text = spannedText
+                    it.maxLines = 2
+                    it.ellipsize = TextUtils.TruncateAt.END
+                },
+            )
+        }
+
+        if (article.author.isNullOrEmpty().not()) {
+            Text(
+                text = article.author ?: "",
+                modifier = Modifier.padding(end = 8.dp, top = 8.dp)
+            )
+        }
+        Text(
+            text = article.publishedAt,
+            fontWeight = FontWeight.Light,
+            fontSize = 12.sp
+        )
+
     }
 }
 
